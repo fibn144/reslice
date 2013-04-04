bad2ac3dcbbfa6d60972fd1fc168e27579e13e5a
Yaron Greenwald <yarong@nx4.priv>
2013-04-03 20:00:44 -0700

    added use of argparse added option of explicitly specifying a list of files on the command line

--- reslice.py
+++ reslice.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import sys
 """
@@ -32,3 +33,32 @@ for line in inlist:
 	print cmd
 	os.system(cmd)
 	os.chdir(homedir)
+
+
+
+
+
+
+
+
+parser = argparse.ArgumentParser(
+    description="""Takes a list of files (if no list is provided looks for a 
+list in the file gzlist) and, for each file, unzips it and calls 
+'reslice <file> -zdim 45'""")
+parser.add_argument("files", nargs="*", help="the files to be processed")
+
+def main(cmdargs):
+    args = parser.parse_args(cmdargs)
+    filelist = []
+    if len(args.files) == 0:
+	    filelist += getfiles(filelistFilename)
+    else:
+	    filelist += args.files
+    filelist = set(filelist)
+
+if __name__ == '__main__':
+    main(sys.argv)
+
+
+# python reslice.py file1.gz file2.gz ...
+# python reslice.py
