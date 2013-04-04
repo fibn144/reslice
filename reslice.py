51c02d3c7aa77bd325238954d7ec63fc35b0d3c2
Yaron Greenwald <yarong@nx4.priv>
2013-04-04 10:01:13 -0700

    Broke up program into smaller chucks. Added ability to specify other input file.

--- reslice.py
+++ reslice.py
@@ -11,54 +11,80 @@ if len(sys.argv) < 2:
 	print ''
 	sys.exit()
 """
-infile = file('gzlist','r')
+# infile = file('gzlist','r')
 
-inlist = [line.split('\n')[0] for line in infile.readlines()]
-homedir = os.path.abspath('.')
-print homedir
-#stop
-for line in inlist:
-	line = line.replace('\"', '')
-	L = line.split('/')
-	targdir = L[1]
-	img = L[2]
-	os.chdir(targdir)
-	if 'gz' in img:
-		cmd = 'gunzip ' + img
-		os.system(cmd)
-		unzip = img.split('.gz')[0]
-	else:
-		unzip = img
-	cmd = 'resliceLattice ' + unzip + ' -zdim 45'
+# inlist = [line.split('\n')[0] for line in infile.readlines()]
+# homedir = os.path.abspath('.')
+# print homedir
+# #stop
+# for line in inlist:
+# 	line = line.replace('\"', '')
+# 	L = line.split('/')
+# 	targdir = L[1]
+# 	img = L[2]
+# 	os.chdir(targdir)
+# 	if 'gz' in img:
+# 		cmd = 'gunzip ' + img
+# 		os.system(cmd)
+# 		unzip = img.split('.gz')[0]
+# 	else:
+# 		unzip = img
+# 	cmd = 'resliceLattice ' + unzip + ' -zdim 45'
+# 	print cmd
+# 	os.system(cmd)
+# 	os.chdir(homedir)
+
+
+def unzip(zipfile):
+	cmd = 'gunzip ' + zipfile
 	print cmd
 	os.system(cmd)
-	os.chdir(homedir)
+	return  zipfile.split('.gz')[-1]
+	
 
 
+def get_files(filelistFilename):
+	infile = file(filelistFilename,'r')
+
+	return [line.split('\n')[0] for line in infile.readlines()]
+	
 
 
-
-
-
+def reslice_image(imgPath):
+	cmd = 'resliceLattice ' + imgPath + ' -zdim 45'
+	print cmd
+	os.system(cmd)
+	
 
 parser = argparse.ArgumentParser(
-    description="""Takes a list of files (if no list is provided looks for a 
-list in the file gzlist) and, for each file, unzips it and calls 
+	description="""Takes a list of files (if no list is provided looks for a 
+list in the file gzlist) and, for each file, unzips it if needed and calls 
 'reslice <file> -zdim 45'""")
-parser.add_argument("files", nargs="*", help="the files to be processed")
+parser.add_argument('files', nargs='*', help='the files to be processed')
+parser.add_argument('-l', '--filelist',
+		    help="""the file containing a list of files to be processed.
+Only read if there are no other files listed.""",
+		    default='gzlist')
 
 def main(cmdargs):
-    args = parser.parse_args(cmdargs)
-    filelist = []
-    if len(args.files) == 0:
-	    filelist += getfiles(filelistFilename)
-    else:
-	    filelist += args.files
-    filelist = set(filelist)
-
+	args = parser.parse_args(cmdargs)
+	filelist = []
+	filelist += args.files
+	if len(args.files) == 0:
+		filelist += get_files(args.filelist)
+	filelist = set(filelist) #removes duplicates
+	currentDir = os.path.abspath('.')
+	print currentDir
+	for image in filelist:
+		image = image.strip('\"')
+		if '.gz' in image:
+			image = unzip(image)
+		reslice_image(image)
+	
 if __name__ == '__main__':
-    main(sys.argv)
+	main(sys.argv[1:])
 
 
 # python reslice.py file1.gz file2.gz ...
 # python reslice.py
+# python reslice.py -l othergzlist
